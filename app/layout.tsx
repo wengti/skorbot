a1a045666be10b0104ffc1493c840d4957fa7841
Weng Ti <wengti@hotmail.com>
2026-03-10 10:17:03 +0800

    further refining template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
-import { ReactNode } from "react";
 import "./globals.css";
+import { ReactNode } from "react";
+import { Lato } from 'next/font/google'
 
-export default function RootLayout({children,}: Readonly<{children: ReactNode}>) {
+const lato = Lato({
+    subsets: ['latin'],
+    weight: ['100', '300', '400', '700', '900'],
+    variable: '--lato',
+    display: 'swap'
+})
+
+export default function RootLayout({children}: Readonly<{children: ReactNode}>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en"  className={lato.variable}>
+      <body className='text-4xl font-extrabold'>
           {children}
       </body>
     </html>
