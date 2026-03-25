2063af0eb41e3235d47adda1612a193c3d622362
Weng Ti <wengti@hotmail.com>
2026-03-25 18:01:39 +0800

    replace cacheComponents

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    // cacheComponents: true,
+    cacheComponents: true,
     images: {
         remotePatterns: [
             new URL('https://lh3.googleusercontent.com/**'),
