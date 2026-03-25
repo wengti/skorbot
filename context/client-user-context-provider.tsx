ad9fa037f46e250e6540e810f859b48c3b484a38
Weng Ti <wengti@hotmail.com>
2026-03-25 17:38:21 +0800

    remove auth listener on unmount

--- context/client-user-context-provider.tsx
+++ context/client-user-context-provider.tsx
@@ -5,7 +5,7 @@ import { ClientUserContextType } from "@/type/auth-type";
 import { useRouter } from "next/navigation";
 import { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
 
-const ClientUserContext = createContext<ClientUserContextType >(undefined!)
+const ClientUserContext = createContext<ClientUserContextType>(undefined!)
 
 export function useClientUserContext() {
     return useContext(ClientUserContext)
@@ -21,24 +21,24 @@ export default function ClientUserContextProvdier({ children }: { children: Reac
     useEffect(() => {
         async function getClientUserData() {
             try {
-                
+
                 const supabase = createClient()
                 const { data, error } = await supabase.auth.getClaims()
                 if (error) throw error
                 else {
                     if (data === null) {
-                        setUserData({id: '', name: '', email: '', picture: ''})
+                        setUserData({ id: '', name: '', email: '', picture: '' })
                     }
 
                     else {
                         const currentUserId = data.claims.sub
                         const { data: fetchedData, error: fetchedError } = await supabase
-                                                                                .from('users')
-                                                                                .select<'*', ClientUserContextType>()
-                                                                                .eq('id', currentUserId)
-                        if(error) throw error
+                            .from('users')
+                            .select<'*', ClientUserContextType>()
+                            .eq('id', currentUserId)
+                        if (error) throw error
                         else if (fetchedData === null) {
-                            setUserData({id: '', name: '', email: '', picture: ''})
+                            setUserData({ id: '', name: '', email: '', picture: '' })
                         }
                         else {
                             setError(null)
@@ -50,24 +50,29 @@ export default function ClientUserContextProvdier({ children }: { children: Reac
             catch (error) {
                 if (error instanceof Error) {
                     setError(error.message)
-                    setTimeout(() => {router.push(`/auth/error?error=${error.message}`)}, 0)
+                    setTimeout(() => { router.push(`/auth/error?error=${error.message}`) }, 0)
                 }
                 else {
                     setError('Unknown error')
                     router.push(`/auth/error`)
-                    setTimeout(() => {router.push(`/auth/error`)}, 0)
+                    setTimeout(() => { router.push(`/auth/error`) }, 0)
                 }
             }
         }
-        
-        
+
+
         getClientUserData()
         const supabase = createClient()
-        const {data: _data} = supabase.auth.onAuthStateChange((event, session) => {
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
             if (event === 'SIGNED_IN' || event === 'SIGNED_OUT') {
                 getClientUserData()
             }
         })
+
+        // Unsubscribe on unmount
+        return () => {
+            subscription.unsubscribe()
+        }
 
     }, [])
 
